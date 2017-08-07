52fdfd04018d7f166793cbbf816f7a56a3f2667f
Gina Antonini <ginamantonini@gmail.com>
2017-08-07 16:36:02 -0500

    got the buttons to work

--- converter.js
+++ converter.js
@@ -1,78 +1,69 @@
 console.log("hello out there!");
 
-//Converts Temps:
+var inputTemp = document.getElementById("tempEntered").value = "";
 
-function celsiusToFarenheit (inputTemperature) {
-	return ((inputTemperature * 1.8) + 32)
+//Funcations that converts Temps:
+
+function celsiusToFarenheit (inputTemp) {
+	return (inputTemp * 1.8) + 32;
 }
 
-function farenheitToCelsius (inputTemperature) {
-	return ((inputTemperature - 32)/ 1.8)
+function farenheitToCelsius (inputTemp) {
+	return (inputTemp - 32) / 1.8;
 }
 
 //function that determines which conversion should happen based on radio button selected (if/else)
 function scale() { 
-	return document.querySelector('input[name="radio"]:checked').value ;
+	return document.querySelector('input[name="radio"]:checked').value;
 }; 
 
 
-function converter (scaleSelected, inputTemperature) {
+function converter (scaleSelected, inputTemp) {
 	if (scaleSelected === "C") {
-		 return celsiusToFarenheit(inputTemperature)
+		 return celsiusToFarenheit(inputTemp)
 	} else { 
-		 return farenheitToCelsius(inputTemperature)
+		 return farenheitToCelsius(inputTemp)
 	}
 }
 
-var inputTemperature = document.getElementById("textInput").value;
-
-
 //function that turns converted temp red if greater than 90F/32C, blue if less than 32F/0C, and for any other temperature-green
 
-function colorChanger (inputTemperature, scaleSelected) {
-if ((scaleSelected === "C" && inputTemperature > 32) || (scaleSelected === "F" && inputTemperature > 90)) {
+function colorChanger (inputTemp, scaleSelected) {
+if ((scaleSelected === "C" && inputTemp > 32) || (scaleSelected === "F" && inputTemp > 90)) {
 		return "red"
-	} else if ((scaleSelected === "C" && inputTemperature < 0) || (scaleSelected === "F" && inputTemperature < 32)) {
+	} else if ((scaleSelected === "C" && inputTemp < 0) || (scaleSelected === "F" && inputTemp < 32)) {
 		return "blue"
 	} else {
 		return "green"
 	}
 }
 
+//function that uses the results of previous functions 
 
 function puppetMaster () {
 	var scaleSelected = scale()
-	var convertedTemp = converter(scaleSelected, inputTemperature)
-	var changedColor = colorChanger(convertedTemp, scaleSelected)
-	console.log(changedColor);
+	var convertedTemp = converter(scaleSelected, inputTemp)
+	// var changedColor = colorChanger(convertedTemp, scaleSelected)
+	console.log(convertedTemp)
 }
 
 
 ///////////////////function to be executed when the converter button is clicked///////////////////////////
 
-var displayButton = document.getElementById("displayConvertedTemp")
+var converterButton = document.getElementById("converterButton")
 
-displayButton.addEventListener('click', puppetMaster)
-
+function displayConvertedTemp () {
+	converterButton.addEventListener('click', puppetMaster);
+	return convertedTemp;
+}
 
 ///////////////////function to be executed when the clear input field button is clicked/////////////////////
 
-function clearButton () {
-	return document.getElementById("clearInputField").value = "";
+function clearFields() {
+     document.getElementById("tempEntered").value = "";
 }
 
-var textInput = document.getElementById("textInput");
-var input = document.querySelector('clearInputField');
-
-
-// input.addEventListener('click', clearButton) {
-// 	console.log("textInput");
-//     input.value = '';
-//     console.log("clear");
-// }
-
-
-
+////////function to make the enter key work/////////////
 
 
 
